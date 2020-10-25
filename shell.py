1f99b0e8fb72c6efee5b20cea8958d2ae6b7b9dd
michaelpineirocode <68714417+michaelpineirocode@users.noreply.github.com>
2020-10-24 19:13:53 -0600

    Update shell.py
    Added an 'echo on' and 'echo off' feature. This does not work as intended right now, but it allows the command to be displayed prior to its output, outside of the input function.

--- shell.py
+++ shell.py
@@ -1,3 +1,19 @@
+import jampy as jam #imports main tokenizer file
+
+ECHO = True
+
 while True:
     text = input("# ")
-    print(text)
+
+    if text == "echo on":
+        ECHO = True
+        continue
+    elif text == "echo off":
+        ECHO = False
+        continue
+
+    if ECHO == True:
+        print(text)
+
+    jam.Tokenizer(text) #pass jam tokenizer
+    
