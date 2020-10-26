141a1ac20a6ca5096854064d433f17c3c978f42f
Michael Pineiro <68714417+michaelpineirocode@users.noreply.github.com>
2020-10-25 21:38:35 -0600

    Update shell.py
    added 'debug' functionality for the future

--- shell.py
+++ shell.py
@@ -1,19 +1,24 @@
 import jampy as jam #imports main tokenizer file
 
 ECHO = True
-
+DEBUG = False
 while True:
     text = input("# ")
 
     if text == "echo on":
-        ECHO = True
+        ECHO = True 
         continue
     elif text == "echo off":
         ECHO = False
+        continue
+    if text == "debug on":
+        DEBUG = True
+        continue
+    elif text == "debug off":
+        DEBUG = False
         continue
 
     if ECHO == True:
         print(text)
 
-    jam.Tokenizer(text) #pass jam tokenizer
-    
+    jam.Tokenizer(text, DEBUG) #pass jam tokenizer
